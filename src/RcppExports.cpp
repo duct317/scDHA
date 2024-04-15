807ed96ce2c4306ae4592c74c6b5986d3093d470
Ha Nguyen <108488221+hanv14@users.noreply.github.com>
2024-04-15 10:05:23 -0500

    Ha - update maintainer

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // DC_para
 List DC_para(NumericMatrix& mat, int k);
 RcppExport SEXP _scDHA_DC_para(SEXP matSEXP, SEXP kSEXP) {
